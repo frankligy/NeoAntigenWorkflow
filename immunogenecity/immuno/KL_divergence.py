f223ea74b0364f9c50b8d85eea306d3c08af01a7
Guangyuan Li <Frank Li>
2020-09-11 18:32:55 -0400

    Kullback-leibler analysis

---
+++ immunogenecity/immuno/KL_divergence.py
@@ -0,0 +1,164 @@
+'''
+This script is for generating KL-divergence per position
+'''
+
+import pandas as pd
+import numpy as np
+import os
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from utils import *
+from sklearn.metrics import roc_curve, auc, precision_recall_curve, confusion_matrix, f1_score,accuracy_score
+import matplotlib.pyplot as plt
+from seperateCNN import *
+import scipy.stats as sc
+import collections
+from ResLikeCNN import *
+
+
+def peptide_and_hla(peptide,hla_type,hla, dic_inventory):
+    length = len(peptide)
+    if length == 10:
+        peptide = peptide
+    elif length == 9:
+        peptide = peptide[:5] + '-' + peptide[5:]
+
+    dic = paratope_dic(hla)
+    try:
+        seq = dic[hla_type]
+    except KeyError:
+        hla_type = rescue_unknown_hla(hla_type, dic_inventory)
+        seq = dic[hla_type]
+
+    total = peptide + seq   # concatenate them together
+    return total
+
+def construct_aa_array(df):
+    slots = np.empty([df.shape[0],56],dtype='<U1')
+    for i in range(df.shape[0]):
+        peptide = df.iloc[i]['peptide']
+        hla_type = df.iloc[i]['HLA']
+        seq = peptide_and_hla(peptide,hla_type,hla,dic_inventory)
+        seq = seq.replace('X','-')
+        slots[i,:] = list(seq)
+    return slots
+
+
+
+def array_to_prob(array):
+
+    length = len(array)
+    counter = collections.Counter(array)
+    return {k:v/length for k,v in counter.items()}
+
+def dic_to_list(dic):
+    # let's set the rule: ARNDCQEGHILKMFPSTWYV-
+    lis = []
+    rule = 'ARNDCQEGHILKMFPSTWYV-'
+    for i in rule:
+        try:
+            lis.append(dic[i])
+        except KeyError:
+            lis.append(0.00001)
+    return np.array(lis)
+
+
+
+
+def convert_to_prob(array):
+    slots = np.empty([21,56],dtype=np.float32)
+    for j in range(array.shape[1]):
+        dic = array_to_prob(array[:,j])
+        lis = dic_to_list(dic)
+        slots[:,j] = lis
+    return slots
+
+
+def KL(neg_prob,pos_prob):
+    slots = np.empty(56,dtype=np.float32)
+    for j in range(neg_prob.shape[1]):
+        neg = neg_prob[:,j]
+        pos = pos_prob[:,j]
+        slots[j] = sc.entropy(pos,neg)
+    return slots
+
+
+def array_to_fasta(array,name):
+    with open(name,'w') as f:
+        for row in array:
+            seq = ''.join(list(row))
+            seq.replace('-','X')
+            f.write('>header\n')
+            f.write('{}\n'.format(seq))
+
+def array_to_fasta_peptide(array,name):
+    with open(name,'w') as f:
+        for row in array:
+            seq = ''.join(list(row))
+
+
+if __name__ == '__main__':
+    hla = pd.read_csv('hla2paratopeTable_aligned.txt',sep='\t',header=None,names=['hla','paratope'])
+    inventory = hla['hla']
+    dic_inventory = dict_inventory(inventory)
+
+    immuno_training = pd.read_csv('data/shuffle_training_test.txt',sep='\t')
+    immuno_val = pd.read_csv('data/shuffle_validation_filter910.txt',sep='\t')
+    immuno = pd.concat([immuno_training,immuno_val])
+    neg,pos = immuno.groupby(by=['immunogenecity'])
+    neg,pos = neg[1],pos[1]
+
+    neg_array = construct_aa_array(neg)
+    pos_array = construct_aa_array(pos)
+    neg_dis = convert_to_prob(neg_array)
+    pos_dis = convert_to_prob(pos_array)
+
+    # compute KL divergence
+    realKL = KL(neg_dis,pos_dis)
+    # draw weblogo
+    array_to_fasta(neg_array,'real_neg.fasta')
+    array_to_fasta(pos_array,'real_pos.fasta')
+
+
+
+    # what the model learned, we use binding data training set
+    ResLikeCNN = model()
+    ResLikeCNN.load_weights('ResLikeCNN_epoch8_sigmoid/')
+    binding = pd.read_csv('data/transfer_training.txt',sep='\t')
+
+    dataset = construct(binding, hla, dic_inventory)   # [ (10,21,1),(46,21,1),(1,1)   ]
+    input1 = pull_peptide(dataset)
+    input2 = pull_hla(dataset)
+    result = ResLikeCNN.predict(x=[input1, input2])
+    hard = [1 if i > 0.5 else 0 for i in result]
+    binding['immunogenecity'] = hard
+    neg_r,pos_r = binding.groupby(by=['immunogenecity'])
+    neg_r,pos_r = neg_r[1],pos_r[1]
+
+    neg_array_r = construct_aa_array(neg_r)
+    pos_array_r = construct_aa_array(pos_r)
+    neg_dis_r = convert_to_prob(neg_array_r)
+    pos_dis_r = convert_to_prob(pos_array_r)
+
+    # compute KL divergence
+    learnedKL = KL(neg_dis_r,pos_dis_r)
+    # draw weblogo
+    array_to_fasta(neg_array_r,'learn_neg.fasta')
+    array_to_fasta(pos_array_r,'learn_pos.fasta')
+
+    # compare
+    plt.bar(np.arange(56)+1,realKL,color='blue',alpha=0.3)
+    plt.bar(np.arange(56)+1, learnedKL, color='orange',alpha=0.3)
+
+    # plot KL plot
+    plt.bar(np.arange(10)+1,realKL[0:10],color='blue',alpha=0.3)
+    plt.xticks(np.arange(10)+1,labels=['P1','P2','P3','P4','P5','P6','P7','P8','P9','P10'])
+    plt.title('Real KL divergence across 10 peptide positions')
+
+    plt.bar(np.arange(10)+1,learnedKL[0:10],color='orange',alpha=0.3)
+    plt.xticks(np.arange(10)+1,labels=['P1','P2','P3','P4','P5','P6','P7','P8','P9','P10'])
+    plt.title('learned KL divergence across 10 peptide positions')
+
+
+
--- result/breast_cancer_analysis.py
+++ result/breast_cancer_analysis.py
@@ -5,6 +5,7 @@ import scipy.stats as sc
 from matplotlib.colors import ListedColormap
 import collections
 
+
 df = pd.read_csv('/Users/ligk2e/Desktop/final.txt',sep='\t',header=None)
 group = pd.read_csv('/Users/ligk2e/Desktop/group.txt',sep='\t')
 
@@ -118,5 +119,63 @@ df_new = df_new.set_index(pd.Index(np.arange(df_new.shape[0])))
 high = df_new.iloc[0:268]
 medium = df_new.iloc[268:536]
 low = df_new.iloc[536:]
+high.to_csv('/Users/ligk2e/Desktop/high.txt',sep='\t',index=None)
+medium.to_csv('/Users/ligk2e/Desktop/medium.txt',sep='\t',index=None)
+low.to_csv('/Users/ligk2e/Desktop/low.txt',sep='\t',index=None)
+
+# AFTER talking with Anu on Sep 10th
+
+# draw 3D scatter plot
+from mpl_toolkits import mplot3d
+df3d = pd.read_csv('/Users/ligk2e/Desktop/final2.txt',sep='\t',header=None)
+ax = plt.axes(projection='3d')
+ax.scatter3D(df3d[1],df3d[3],df3d[2])
+ax.set_xlabel('Neojunction count per patient')
+ax.set_ylabel('Neojunctions that give rise to neoantigens')
+ax.set_zlabel('Neoantigen count')
+
+
+# recurrent event make them unique
+events = pd.read_csv('/Users/ligk2e/Desktop/all_events.txt')
+events_unique = list(set(list(events['UID'].values)))   # 10270 events
+cond = [False if item == 'UID' else True for item in events['UID']]
+count = collections.Counter(events['UID'].loc[cond])
+n500 = count.most_common(4000)
+n500_array = np.array(n500)
+df = pd.DataFrame({'event':n500_array[:,0],'occurence':n500_array[:,1]})
+df.to_csv('/Users/ligk2e/Desktop/events4000.txt',sep='\t',index=None)
+
+
+## after running on linux cluster, we get average neoantigen generations for each recurrent event
+ave = pd.read_csv('/Users/ligk2e/Desktop/tmp13.txt',sep='\t',header=None)
+ave.columns = ['event','occurence','average-neoantigen-generation']
+ave.to_csv('/Users/ligk2e/Desktop/recurrent.txt',sep='\t',index=None)
+
+# interquartile range and outliers
+plt.boxplot(df[2])
+x = np.random.normal(1,0.02,len(df[2]))
+plt.plot(x,df[2],'r.',alpha=0.2)
+plt.title('Neoantigen counts distribution')
+
+q1 = df[2].quantile(q=0.25)
+q3 = df[2].quantile(q=0.75)
+iqr = q3-q1
+upper_whisker = q3 + 1.5*iqr
+lower_whisker = q1 - 1.5*iqr
+# [lower_whisker, q1, median, q3, upper_whisker]
+label = []
+for i in range(df.shape[0]):
+    if df[2].iloc[i] > lower_whisker and df[2].iloc[i] <= q1:
+        label.append('low')
+    elif df[2].iloc[i] > q1 and df[2].iloc[i] <= q3:
+        label.append('mediem')
+    elif df[2].iloc[i] > q3 and df[2].iloc[i] <= upper_whisker:
+        label.append('high')
+    else:
+        label.append('outliers')
+df['label'] = label
+df.columns = ['TCGA-ID','neojunction-count','neoantigen-count','label']
+df.to_csv('/Users/ligk2e/Desktop/stratification.txt',sep='\t',index=None)
+
 
 
